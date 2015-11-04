b6a029ef280d19a62ae8c6866703133cc122796c
Yoshio Fujimoto <yosh.fuj@gmail.com>
2015-11-04 11:52:12 -0500

    Total open seats and more classes

--- bot.js
+++ bot.js
@@ -1,13 +1,6 @@
 var request = require("request");
 var cheerio = require('cheerio');
 
-// $ = cheerio.load('<h2 class="title">Hello world</h2>');
- 
-// $('h2.title').text('Hello there!');
-// $('h2').addClass('welcome');
- 
-// $.html();
-
 function getAvailability(course, url) {
 	request({
 	  uri: url
@@ -18,14 +11,18 @@ function getAvailability(course, url) {
  
 		var $ = cheerio.load(body); // Initiate cheerio object
 
+		var seats = 0;
+
 		$('.sections-container .section .section-info-container .seats-info-group .open-seats .open-seats-count').each(function(i, element) {
-			console.log(course + ": " + $(this).text());
-		})
+			seats += parseInt($(this).text())
+		})	
+
+		console.log(course + ": " + seats);
 	});
 }
 
 function runScript () {
-	var classes = ["CMSC422", "CMSC436"];
+	var classes = ["CMSC422", "CMSC436", "CMSC412", "CMSC414", "CMSC417", "CMSC421", "CMSC424", "CMSC430", "CMSC433", "CMSC434"];
 
     for (var i = 0; i < classes.length; i++) {
     	getAvailability(classes[i], "https://ntst.umd.edu/soc/search?courseId=" + classes[i] + "&sectionId=&termId=201601&_openSectionsOnly=on&courseLevelFilter=ALL&instructor=&teachingCenter=ALL&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&creditCompare=&credits=&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on");
@@ -33,30 +30,6 @@ function runScript () {
 }
 
 setInterval( function(){
-	console.log("Open Seats:");
+	console.log("Total open Seats:");
 	runScript()
-}, 5000);
-
-
-// var url = "https://ntst.umd.edu/soc/search?courseId=CMSC436&sectionId=&termId=201601&_openSectionsOnly=on&courseLevelFilter=ALL&instructor=&teachingCenter=ALL&courseStartCompare=&courseStartHour=&courseStartMin=&courseStartAM=&courseEndHour=&courseEndMin=&courseEndAM=&creditCompare=&credits=&_classDay1=on&_classDay2=on&_classDay3=on&_classDay4=on&_classDay5=on";
-
-// request({
-// 	  uri: url
-// 	}, function(error, response, body) {
-// 		if (error != null) {
-// 			return;
-// 		}
- 
-// 		var $ = cheerio.load(body); // Initiate cheerio object
-
-// 		$('.sections-container .section .section-info-container .seats-info-group .open-seats .open-seats-count').each(function(i, element) {
-// 			console.log($(this).text());
-// 		})
-// 	});
-
-// var $ = cheerio.load(body); // Initiate cheerio object
-
-// $('.sections-container .section').each(function(i, element) {
-// 	console.log(element);
-// })	
-
+}, 60000);
